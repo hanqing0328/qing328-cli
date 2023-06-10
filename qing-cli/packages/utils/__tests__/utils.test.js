6199441e67f55aad6b489872e96a1b1702efb337
qing <1126643825@qq.com>
2023-06-11 01:07:32 +0800

    re innit

--- qing-cli/packages/utils/__tests__/utils.test.js
+++ qing-cli/packages/utils/__tests__/utils.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const utils = require('..');
-const assert = require('assert').strict;
 
-assert.strictEqual(utils(), 'Hello from utils');
-console.info('utils tests passed');
+describe('utils', () => {
+    it('needs tests');
+});
